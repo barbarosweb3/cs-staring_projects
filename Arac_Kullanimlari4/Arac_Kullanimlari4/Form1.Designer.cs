d28207fb77d7c6d393ca678a234e7dcaae41bac1
Barbaros Emre Alagöz <barbaros@web3wanderers.com>
2024-12-28 16:10:26 +0300

    Use Of Tools

---
+++ Arac_Kullanimlari4/Arac_Kullanimlari4/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Arac_Kullanimlari4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.Kadın = new System.Windows.Forms.RadioButton();
+            this.Erkek = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(66, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 189);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Soslar";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(46, 38);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 28);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Mayonez";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(46, 72);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(93, 28);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Ketçap";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(46, 106);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(93, 28);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Hardal";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Kadın
+            // 
+            this.Kadın.AutoSize = true;
+            this.Kadın.Location = new System.Drawing.Point(29, 31);
+            this.Kadın.Name = "Kadın";
+            this.Kadın.Size = new System.Drawing.Size(84, 28);
+            this.Kadın.TabIndex = 1;
+            this.Kadın.TabStop = true;
+            this.Kadın.Text = "Kadın";
+            this.Kadın.UseVisualStyleBackColor = true;
+            // 
+            // Erkek
+            // 
+            this.Erkek.AutoSize = true;
+            this.Erkek.Location = new System.Drawing.Point(29, 65);
+            this.Erkek.Name = "Erkek";
+            this.Erkek.Size = new System.Drawing.Size(84, 28);
+            this.Erkek.TabIndex = 2;
+            this.Erkek.TabStop = true;
+            this.Erkek.Text = "Erkek";
+            this.Erkek.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Cyan;
+            this.panel1.Controls.Add(this.Kadın);
+            this.panel1.Controls.Add(this.Erkek);
+            this.panel1.Location = new System.Drawing.Point(371, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 181);
+            this.panel1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(716, 44);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(302, 181);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1200, 675);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton Kadın;
+        private System.Windows.Forms.RadioButton Erkek;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
