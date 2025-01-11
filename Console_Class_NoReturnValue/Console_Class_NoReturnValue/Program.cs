48f931c34dbb04450f5e157d6e57d862cf128fea
Barbaros Emre Alagöz <barbaros@web3wanderers.com>
2025-01-11 12:32:05 +0300

    Class on Console (void)

---
+++ Console_Class_NoReturnValue/Console_Class_NoReturnValue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Class_NoReturnValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mesaj msj = new Mesaj();
+            msj.metin();
+            Console.Read();
+        }
+    }
+}
