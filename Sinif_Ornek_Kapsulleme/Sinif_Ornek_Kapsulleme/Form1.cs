e87dcf51c8502c31dd6c04ed7bb228ace4301fdf
Barbaros Emre Alagöz <barbaros@web3wanderers.com>
2025-01-11 11:51:04 +0300

    Class Capsulation

---
+++ Sinif_Ornek_Kapsulleme/Sinif_Ornek_Kapsulleme/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinif_Ornek_Kapsulleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Araba rb = new Araba();
+
+            rb.renk = "Yeşil";
+            rb.hiz = 200;
+            rb.motor = 1300;
+            rb.fiyat = 50000;
+            rb.durum = 'y';
+            rb.YIL = 2024;
+            rb.MARKASI = "mercedes";
+
+            label1.Text = rb.renk;
+            label2.Text = rb.hiz.ToString();
+            label3.Text = rb.motor.ToString();
+            label4.Text = rb.fiyat.ToString();
+            label5.Text = rb.durum.ToString();
+            label6.Text = rb.YIL.ToString();
+            label7.Text = rb.MARKASI;
+
+            pictureBox1.BackColor = Color.CadetBlue;
+
+        } 
+    }
+}
---
+++ Sinif_Ornek_Kapsulleme/Sinif_Ornek_Kapsulleme/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinif_Ornek_Kapsulleme
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Araba rb = new Araba();
+
+            rb.renk = "Mavi";
+            rb.hiz = 160;
+            rb.motor = 1600;
+            rb.fiyat = 40000;
+            rb.durum = 'i';
+            rb.YIL = 2019;
+            rb.MARKASI = "renault";
+
+            label1.Text = rb.renk;
+            label2.Text = rb.hiz.ToString();
+            label3.Text = rb.motor.ToString();
+            label4.Text = rb.fiyat.ToString();
+            label5.Text = rb.durum.ToString();
+            label6.Text = rb.YIL.ToString();
+            label7.Text = rb.MARKASI;
+
+            pictureBox1.BackColor = Color.SeaGreen;
+        }
+    }
+}
