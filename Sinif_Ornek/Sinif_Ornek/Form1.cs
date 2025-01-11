2ff4197e7fd69baa24852ccd2bc205761d78e583
Barbaros Emre Alagöz <barbaros@web3wanderers.com>
2025-01-11 11:17:51 +0300

    Class and Example

---
+++ Sinif_Ornek/Sinif_Ornek/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinif_Ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Araba rb = new Araba();
+            rb.renk = "Mavi";
+            rb.hiz = 160;
+            rb.motor = 1245.56;
+            rb.fiyat = 50000;
+            rb.durum = 's';
+
+            label1.Text = rb.renk;
+            label2.Text = rb.hiz.ToString();
+            label3.Text = rb.motor.ToString();
+            label4.Text = rb.fiyat.ToString();
+            label5.Text = rb.durum.ToString();
+            pictureBox1.BackColor = Color.CadetBlue;
+        }
+    }
+}
