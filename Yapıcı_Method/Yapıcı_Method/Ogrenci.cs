61dcd5cd25d4508f9ff542f41cf193a7076976c0
Barbaros Emre Alagöz <barbaros@web3wanderers.com>
2025-01-11 14:23:57 +0300

    Constructor_Method

---
+++ Yapıcı_Method/Yapıcı_Method/Ogrenci.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yapıcı_Method
+{
+    internal class Ogrenci
+    {
+        public Ogrenci(string ad, string soyad, int yas, string meslek)
+        {
+            //Console.WriteLine("Durum: " + bilgi);
+            Console.WriteLine("Durum: " + ad + " " + soyad + " " + yas + " " + meslek);
+        }
+    }
+}
---
+++ Yapıcı_Method/Yapıcı_Method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yapıcı_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad, soyad, yas, meslek;
+            Console.Write("Ad: ");
+            ad = Console.ReadLine();
+            Console.Write("Soyad: ");
+            soyad = Console.ReadLine();
+            Console.Write("Yaş: ");
+            yas = Console.ReadLine();
+            Console.Write("Meslek: ");
+            meslek = Console.ReadLine();
+            //Ogrenci ogr = new Ogrenci(blg);
+            Ogrenci ogr = new Ogrenci(ad, soyad, Convert.ToInt16(yas), meslek);
+            Console.Read();
+        }
+    }
+}
