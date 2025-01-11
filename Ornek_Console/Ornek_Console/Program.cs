4ae17817f5e8c0f4d9fe24d121ed3db82f04e4fb
Barbaros Emre Alagöz <barbaros@web3wanderers.com>
2025-01-11 12:25:50 +0300

    Console App

---
+++ Ornek_Console/Ornek_Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Merhaba");
+            //Console.Write("Dünya");
+            //string kelime;
+            //kelime = "26 Ekim";
+            //Console.WriteLine(kelime);
+            int sayi;
+            sayi = 5;
+            Console.WriteLine(sayi);
+            Console.Read();
+        }
+    }
+}
