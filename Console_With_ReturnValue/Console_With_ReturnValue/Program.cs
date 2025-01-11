2237eff6272c658c9db63bc265aacb59a7565033
Barbaros Emre Alagöz <barbaros@web3wanderers.com>
2025-01-11 14:02:10 +0300

    Console_with_ReturnValue

---
+++ Console_With_ReturnValue/Console_With_ReturnValue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_With_ReturnValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            islem isl = new islem();
+            isl.topla(7, 6);
+          
+            Console.WriteLine("\n\n\n");
+
+            isl.kare(9);
+
+            Console.WriteLine("\n\n\n");
+
+            isl.toplam(5,8);
+            Console.Read();
+        }
+    }
+}
---
+++ Console_With_ReturnValue/Console_With_ReturnValue/islem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Console_With_ReturnValue
+{
+    internal class islem
+    {
+        public int topla(int s1, int s2) 
+        {
+            int s3 = s1 + s2;
+            Console.Write("Sonuc: " + s3);
+            return s3;
+        }
+
+        public int kare(int deger)
+        {
+            int sonuc = deger * deger;
+            Console.WriteLine("Sonuç: " + sonuc);
+            return sonuc;
+        }
+
+        public int toplam(int num1, int num2)
+        {
+            Console.Write("Bir sayı giriniz: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("İkinci sayıyı giriniz: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = num1 + num2;
+            Console.WriteLine("Sonuc: " + num3);
+            return num3;
+            
+        }
+    }
+}
