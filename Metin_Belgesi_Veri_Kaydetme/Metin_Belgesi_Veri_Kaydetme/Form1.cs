80d48209d6073b50c5b30516e4000e21ec1ed25f
Barbaros Emre Alagöz <barbaros@web3wanderers.com>
2025-01-12 06:43:30 +0300

    Saving Data via TextBox

---
+++ Metin_Belgesi_Veri_Kaydetme/Metin_Belgesi_Veri_Kaydetme/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Metin_Belgesi_Veri_Kaydetme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Metin Dosyası | *.txt";
+            saveFileDialog1.Title = "Metin Dosyası Kaydet";
+            saveFileDialog1.ShowDialog();
+            StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+            sw.WriteLine(richTextBox1.Text);
+            sw.Close();
+            MessageBox.Show("Metin Dosyası Kaydedildi.");
+        }
+    }
+}
